e1353d50d30723ca47f7081e40527d5445ac3ed7
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-10-08 15:05:47 +0200

    Add project files.

---
+++ CounterClass/Counter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterClass
+{
+    internal class Counter
+    {
+         int counter = 0;
+        const int MaxCount = 10;
+
+        public Counter(int start)
+        {
+            counter += start;
+        }
+        
+        // Ökar räknaren med ett
+        public void Increment()
+        {
+            counter++;
+            ReachedMaxCount();
+        }
+
+        // Ökar räknaren med ett visst tal
+        public void IncrementBy(int increment)
+        {
+            counter += increment;
+            ReachedMaxCount();
+        }
+
+        // Återställer räknaren
+        public void Reset()
+        {
+            counter = 0;
+        }
+
+        // ger värdet på räknaren
+        public int GetValue()
+        {
+            return counter;
+        }
+
+        public void ReachedMaxCount()
+        {
+            if (counter > MaxCount)
+            {
+                counter = 0;
+            }
+        }
+    }
+}
---
+++ CounterClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Startar en ny räknare som börjar på '5'
+            Counter counter = new Counter(0);
+
+            // Höjer räknaren med två istället för ett
+            counter.IncrementBy(2);
+
+            // höjer räknaren med ett
+            counter.Increment();
+            counter.Increment();
+            counter.Increment();
+            // ska skriva ut 3
+            Console.WriteLine("counter: " + counter.GetValue());
+            counter.Reset();
+            // ska skriva ut 0
+            Console.WriteLine("Reset counter: " + counter.GetValue());
+            
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.LeftArrow)
+                {
+                    counter.Increment();
+                    Console.Clear();
+                    Console.WriteLine("Counter: " + counter.GetValue());
+                }
+            }
+        }
+    }
+}
